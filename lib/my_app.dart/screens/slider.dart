0a789267738d283b50a663bb87b7b05390a8eb42
Gulnura Pazulova <gulnura.pazulova.pls@gmail.com>
2022-05-16 11:47:41 +0600

    git

--- lib/my_app.dart/screens/slider.dart
+++ lib/my_app.dart/screens/slider.dart
@@ -3,25 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 
-
-// final images = [
-//   Image.asset('images/camera.jpg'),
-//   Image.asset('images/logo.png'),
-//   Image.asset('images/about.pngg'),
-//   // Image.asset('images/burger_4.png'),
-// ];
-
 class ImageSilderDemo extends StatelessWidget {
   final images = [
   Image.asset('images/camera.jpg'),
-  // Image.asset('images/logo.png'),
   Image.asset('images/about.png'),
-  // Image.asset('images/burger_4.png'),
 ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // backgroundColor: Colors.black,
+
+
+      
       appBar: AppBar(
         title: Text('Images Scroll'),
       ),
